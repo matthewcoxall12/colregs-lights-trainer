f6865ea021e165e5053b813f7ac4da3ed9e85cae
matthewcoxall12 <matthewcoxall@googlemail.com>
2025-05-12 21:19:45 +0100

    Initial Upload
    Initial Upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+// Component import
+import LightsGrid from './components/LightsGrid';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="app-content">
+        <LightsGrid />
+      </main>
+    </div>
+  );
+}
+
+export default App;
